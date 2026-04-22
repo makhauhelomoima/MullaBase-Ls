711689e46b795cd17d3d713cff4b719d9c0aff62
makhauhelomoima <makhauhelomoima@gmail.com>
2026-04-22 07:55:34 +0200

    Create page.js

---
+++ app/store/page.js
@@ -0,0 +1,13 @@
+'use client'
+export default function Store() {
+  return (
+    <main className="min-h-screen bg-white text-black text-sm">
+      <div className="border-b border-black p-2 flex justify-between"><span className="font-bold">CREATE YOUR STORE</span><a href="/engage" className="border border-black px-2">Back</a></div>
+      <div className="p-4 max-w-sm mx-auto border border-black text-center">
+        <div className="font-bold mb-2">Open Blog for Store Owners</div>
+        <div className="text-xs">Post your store link. Users visit & support.</div>
+        <div className="text-xs mt-2">*Store link posting coming soon</div>
+      </div>
+    </main>
+  )
+    }
